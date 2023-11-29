12415c797b98e87b8edeece21a494b42b1946d75
shubham joshi <joshi.shubham7112000@gmail.com>
2023-11-29 16:07:48 +0530

    create sign in api route

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,12 +1,11 @@
-import { Link , useNavigate} from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { useState } from 'react'
 
 export default function SignUp() {
   const [formData, setFormData] = useState({})
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
-
+  const navigate = useNavigate()
   const handleChange = (e) => {
     setFormData(
       {
@@ -17,30 +16,32 @@ export default function SignUp() {
 
   const handleSubmit = async (e) => {
     e.preventDefault()
-    try {setLoading(true);
-      const res = await fetch('/api/auth/signup',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      });
-      const data = await res.json();
-      console.log(data);
-      if(data.success === false){
-        setLoading(false);
-        setError(data.message);
-        return;
-      }
+    try {
+      setLoading(true);
+    const res = await fetch('/api/auth/signup',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    });
+    const data = await res.json();
+    console.log(data);
+    if(data.success === false) {
       setLoading(false);
-      setError(null);
-      navigate('/sign-in');
+      setError(data.message);
+    
+       return;
+    }
+    setLoading(false);
+    setError(null);
+    navigate('/sign-in');
     } catch (error) {
       setLoading(false);
       setError(error.message);
-    }
-  };
+    } 
+  }
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
@@ -54,7 +55,7 @@ export default function SignUp() {
         <input type="password" placeholder='password'
         className='border p-3 rounded-lg' id='password' onChange={handleChange}/>
 
-        <button disabled={Loading} className='bg-slate-700 text-white p-3 
+        <button disabled={loading}className='bg-slate-700 text-white p-3 
         rounded-lg uppercase hover:opacity-95
         disabled:opacity-80'>
           {loading ? 'Loading...' : 'Sign Up'}
@@ -66,7 +67,8 @@ export default function SignUp() {
           <span className='text-blue-700'></span>
         </Link>
       </div>
-      {error && <p className='text-red-500 mt-5'>{error}</p>}
+      {error && <p className='text-red-500 mt-5'>{error} </p>}
     </div>
   )
 }
+
