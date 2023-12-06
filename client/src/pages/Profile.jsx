6d0e6e8a151baf57e3f44962aa0b9c2485b79708
shubham joshi <joshi.shubham7112000@gmail.com>
2023-12-06 21:09:02 +0530

    add signOut functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,13 +1,19 @@
 import {useSelector} from 'react-redux'
 import { useRef, useState, useEffect } from 'react'
-import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { 
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable } from 'firebase/storage';
 import { app } from '../firebase';
-import {updateUserStart,
+import {
+  updateUserStart,
   updateUserSuccess, 
   updateUserFailure,
    deleteUserFailure,
    deleteUserStart,
-   deleteUserSuccess
+   deleteUserSuccess,
+   signOutUserStart
  } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 
@@ -104,9 +110,25 @@ export default function Profile() {
    } catch (error) {
       dispatch(deleteUserFailure(error.message))
    }
+  }
+  const handleSignOut = async() => {
+   
+   try {
+    dispatch(signOutUserStart())
+    const res = await fetch('/api/auth/signout');
+    const data = await res.json();
+    if (data.success === false){
+      dispatch(deleteUserFailure(data.message));
+      return;
+    }
+    dispatch(deleteUserSuccess(data));
+   } catch (error) {
+    dispatch(deleteUserFailure(data.message));
+   }
 
 
   }
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center
@@ -148,7 +170,7 @@ filePerc > 0 && filePerc < 100 ? (
       </form>
       <div className="flex justify-between mt-5">
         <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete Account</span>
-        <span className='text-red-700 cursor-pointer'>Sign Out</span>
+        <span onClick={handleSignOut} className='text-red-700 cursor-pointer'>Sign Out</span>
   </div>
 
   <p className='text-red-700 mt-5'>{error ? error : ''}</p>
