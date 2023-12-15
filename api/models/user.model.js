3315cfacfa2683effe3d6c4ab683b13ae1652b6a
shubham joshi <joshi.shubham7112000@gmail.com>
2023-12-15 22:47:14 +0530

    completed project

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -15,11 +15,11 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    avtar:{
+    avatar:{
        type: String,
-       default: "C:\Users\SHUBHAM JOSHI\OneDrive\Desktop\profileimage.png"
+       default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
 
-    }
+    },
 }, {timestamps: true})
 
 const User = mongoose.model('User', userSchema)
